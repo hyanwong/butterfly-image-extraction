8c5e906079ff607d6b3e912ae9a05a56823b2088
Yan Wong <yan@yanwong.me>
2014-11-10 11:39:41 +0000

    spelling correction in comments

--- butterfly_comparisons.py
+++ butterfly_comparisons.py
@@ -1,4 +1,4 @@
-'''Call this script as "python butterfly_comparisons.py" to download (if necessary) and run comparisons on all the images in "est_butterfly.csv"
+'''Call this script as "python butterfly_comparisons.py" to download (if necessary) and run comparisons on all the images in "test_butterfly.csv"
    call this script as "python butterfly_comparisons.py save" to save the best outlines to an image_dir, or
    call this script as "python butterfly_comparisons.py imagefile_orig.jpg" to display to screen the best outline and a tiled image of the fitting steps'''
 from __future__ import division
